ee0878a8f7979ea1caed7c59f1fb64585ecdc512
Daniel Ternyak <dternyak@gmail.com>
2018-09-30 02:17:38 -0500

    comment

--- db.py
+++ db.py
@@ -5,6 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+# TODO - determine how to tell IDEs like PyCharm not to delete this
+from api.comment.models import *
+from api.user.models import *
+
 
 def init_db(engine_data: EngineData):
     Base.metadata.create_all(engine_data.engine)
