256e26b89bb72e4a56f4063cc3f340b1f1bd330b
Daniel Ternyak <dternyak@gmail.com>
2018-09-30 03:33:49 -0500

    create shell task

--- tasks.py
+++ tasks.py
@@ -8,11 +8,17 @@ def test(c, cov=False, verbose=False):
         pytest_command += ' --cov=api'
     if verbose:
         pytest_command += ' -s '
-    c.run(pytest_command)
+    c.run(pytest_command, pty=True)
 
 
 @task
-def start(c, log_level='info'):
-    gunicorn_command = 'export ENVIRONMENT=common && gunicorn --reload app:app'
+def start(c, log_level='info', port=5000):
+    gunicorn_command = 'export ENVIRONMENT=common && export PORT={} && gunicorn --reload app:app'.format(port)
     gunicorn_command += ' --log-level={}'.format(log_level)
-    c.run(gunicorn_command)
+    c.run(gunicorn_command, pty=True)
+
+
+@task
+def shell(c):
+    command = 'export ENVIRONMENT="common" && ./scripts/interpreter'
+    c.run(command, pty=True)
